5c24e3749d339b78398ec7b781d1b44e82ff03d7
@m.andriichuk <m.andriichuk.analityk1@gmail.com>
2024-08-20 17:54:31 +0300

    fix update person list in eqItem

--- src/controllers/tours/updateEquipItem.js
+++ src/controllers/tours/updateEquipItem.js
@@ -5,44 +5,73 @@ const updateEquipItem = async (req, res) => {
 	const { _id: ownerId } = req.user
 	const { equipId } = req.body
 	const { id: tripId } = req.params
-	console.log(`id user:`, ownerId)
-	console.log(`id equip:`, equipId)
-	console.log(`id trip:`, tripId)
 	const tour = await ToursModel.findOne({ id: tripId })
 	if (!tour) {
 		throw new HttpError(404, 'trip not found')
 	}
 	const equipList = tour.equipList[0]
 	const equipitem = equipList.find((el) => String(el._id) === equipId)
-	// const index = equipitem.persons.findIndex((el) => String(el) === ownerId)
-	// console.log(index)
-	// if (index !== -1) {
-	// 	equipitem.persons.splice(index, 1)
-	// 	return
-	// }
-	// equipitem.persons.push(ownerId)
+	const indexItem = equipList.findIndex((el) => el._id === equipitem._id)
 	const index = equipitem.persons.findIndex(
 		(el) => String(el) === String(ownerId)
 	)
-	console.log(equipitem.persons.map((el) => el))
-	console.log(ownerId)
+	console.log(index)
 	if (index !== -1) {
-		equipitem.persons.splice(index, 1)
-	} else {
-		equipitem.persons.push(ownerId)
+		equipList.splice(indexItem, 1)
+		const updatedPersons = equipitem.persons.filter(
+			(el) => String(el) !== String(ownerId)
+		)
+		equipitem.persons = updatedPersons
+		const updatedList = [...equipList, equipitem]
+
+		const updatedTour = await ToursModel.findByIdAndUpdate(
+			{ _id: tripId },
+			{
+				equipList: [updatedList]
+			},
+			{ new: true }
+		)
+		res.setHeader('Access-Control-Allow-Credentials', true)
+		res.setHeader('Access-Control-Allow-Origin', '*')
+		// another common pattern
+		// res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+		res.setHeader(
+			'Access-Control-Allow-Methods',
+			'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+		)
+		res.setHeader(
+			'Access-Control-Allow-Headers',
+			'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+		)
+		if (req.method === 'OPTIONS') {
+			res.status(200).end()
+			return
+		}
+		return res.json({
+			code: 200,
+			resp: updatedTour
+		})
 	}
-	console.log(equipitem)
-	const itemindex = equipList.findIndex((el) => el.id === equipitem.id)
-	equipList.splice(itemindex, 1, equipitem)
+	equipList.splice(indexItem, 1)
+	equipitem.persons.push(ownerId)
+	const updatedList = [...equipList, equipitem]
 
 	const updatedTour = await ToursModel.findByIdAndUpdate(
 		{ _id: tripId },
 		{
-			equipList: [equipList]
+			equipList: [updatedList]
 		},
 		{ new: true }
 	)
-	console.log(updatedTour)
+	console.log(equipitem)
+	// const updatedTour = await ToursModel.findByIdAndUpdate(
+	// 	{ _id: tripId },
+	// 	{
+	// 		equipList: [equipList]
+	// 	},
+	// 	{ new: true }
+	// )
+	// console.log(updatedTour)
 	res.setHeader('Access-Control-Allow-Credentials', true)
 	res.setHeader('Access-Control-Allow-Origin', '*')
 	// another common pattern
