d60089417f264252dc4a419eed4d0a342bcfa1f3
@m.andriichuk <m.andriichuk.analityk1@gmail.com>
2024-08-20 14:20:24 +0300

    change get by id trip

--- src/controllers/tours/getById.js
+++ src/controllers/tours/getById.js
@@ -3,9 +3,28 @@ const { HttpError } = require('../../helpers')
 
 const getById = async (req, res) => {
 	const { id } = req.params
+	const { filtered } = req.query
+	// console.log(searchParams)
 	const resp = await ToursModel.findById({ _id: id })
+	console.log(resp)
 	if (!resp) {
 		throw new HttpError(404, 'trip not found')
+	}
+	if (JSON.parse(filtered)) {
+		const filteredByCategory = resp.equipList.flat().reduce((acc, item) => {
+			const category = item.category
+
+			if (!acc[category]) {
+				acc[category] = []
+			}
+
+			acc[category].push(item)
+			return acc
+		}, {})
+		res.json({
+			code: 200,
+			trip: filteredByCategory
+		})
 	}
 	res.json({
 		code: 200,
