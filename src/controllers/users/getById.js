52e498d6646cef40f91cb9522ba0d8d6ef180df5
@m.andriichuk <m.andriichuk.analityk1@gmail.com>
2024-08-21 17:14:14 +0300

    fix get by id

--- src/controllers/users/getById.js
+++ src/controllers/users/getById.js
@@ -2,9 +2,13 @@ const { UserModel } = require('../../models/users')
 
 const getUsersById = async (req, res) => {
 	const { ids } = req.body
+	if (!ids || !ids.length) {
+		return res.json({
+			code: 200,
+			resp: []
+		})
+	}
 	const arrayOfId = ids.split(', ')
-	console.log(arrayOfId)
-	const testArray = ['66a0aa6f77f4e271141859fe']
 	const resp = await UserModel.find(
 		{ _id: { $in: arrayOfId } },
 		{ name: 1, email: 1, _id: 1 }
