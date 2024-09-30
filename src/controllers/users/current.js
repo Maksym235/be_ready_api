93ead1abdba0e160e1fb55689365a197c38d8d96
@m.andriichuk <andriichukwork235@gmail.com>
2024-09-30 15:50:08 +0300

    adds password to current func

--- src/controllers/users/current.js
+++ src/controllers/users/current.js
@@ -1,33 +1,34 @@
 const current = async (req, res) => {
-  const { token, name, id, email, theme, language } = req.user;
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
+	const { token, name, id, email, theme, language, password } = req.user
+	res.setHeader('Access-Control-Allow-Credentials', true)
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	// another common pattern
+	// res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+	)
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+	)
+	if (req.method === 'OPTIONS') {
+		res.status(200).end()
+		return
+	}
 
-  const user = {
-    email,
-    id,
-    name,
-    theme,
-    language,
-  };
-  res.json({
-    user,
-    token,
-  });
-};
+	const user = {
+		email,
+		id,
+		name,
+		password,
+		theme,
+		language
+	}
+	res.json({
+		user,
+		token
+	})
+}
 
-module.exports = current;
+module.exports = current
