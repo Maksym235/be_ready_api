faba071571aed5f2ab57d025fd945b40a54b6f66
Maksym235 <andriichukworl235@gmail.com>
2024-10-03 21:37:43 +0300

    change reset avatar func

--- src/controllers/users/resetToDefaultAvatar.js
+++ src/controllers/users/resetToDefaultAvatar.js
@@ -1,28 +1,28 @@
 const { UserModel } = require('../../models/users')
-const path = require('path')
+// const path = require('path')
 const { cloudinary } = require('../../helpers')
 const resetToDefaultAvatar = async (req, res) => {
 	const { _id: userId } = req.user
-	const avatarsPath = path.join('public', 'avatars')
-	const newPath = path.join(avatarsPath, 'default_user.png')
-	const resultUpload = await cloudinary.uploader.upload(newPath, {
-		folder: 'avatars'
-		// transformation: {
-		// 	height: 120,
-		// 	gravity: 'face',
-		// 	crop: 'thumb',
-		// 	aspect_ratio: 5 / 6,
-		// 	zoomed: 0.75
-		// }
-	})
-	const cloudinaryURL = resultUpload.secure_url
-	const avatarName = resultUpload.original_filename
-	console.log(resultUpload)
+	// const avatarsPath = path.join('public', 'avatars')
+	// const newPath = path.join(avatarsPath, 'default_user.png')
+	const defaultAvatarData = await cloudinary.api.resources_by_tag('default')
+	// const resultUpload = await cloudinary.uploader.upload(newPath, {
+	// 	folder: 'avatars'
+	// 	// transformation: {
+	// 	// 	height: 120,
+	// 	// 	gravity: 'face',
+	// 	// 	crop: 'thumb',
+	// 	// 	aspect_ratio: 5 / 6,
+	// 	// 	zoomed: 0.75
+	// 	// }
+	// })
+	console.log(defaultAvatarData)
+	const cloudinaryURL = defaultAvatarData.resources[0].secure_url
 	await UserModel.findByIdAndUpdate(
 		{ _id: userId },
 		{
 			avatarURL: cloudinaryURL,
-			avatarName
+			avatarName: 'default_user'
 		}
 	)
 
