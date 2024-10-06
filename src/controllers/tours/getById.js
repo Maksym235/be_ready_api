bdadd892f8b99b000a6632c200a2591e9c230060
Maksym235 <andriichukworl235@gmail.com>
2024-10-06 14:08:03 +0300

    change get by id trip

--- src/controllers/tours/getById.js
+++ src/controllers/tours/getById.js
@@ -1,41 +1,26 @@
 const { ToursModel } = require('../../models/tours')
+const { EquipsListModel } = require('../../models/equipList')
 const { HttpError } = require('../../helpers')
 
 const getById = async (req, res) => {
 	const { id } = req.params
-	const { filtered } = req.query
 	// console.log(searchParams)
 	const resp = await ToursModel.findById({ _id: id })
 	console.log(resp)
 	if (!resp) {
 		throw new HttpError(404, 'trip not found')
 	}
-	if (JSON.parse(filtered)) {
-		const filteredByCategory = resp.equipList.flat().reduce((acc, item) => {
-			const category = item.category
-
-			if (!acc[category]) {
-				acc[category] = []
-			}
-
-			acc[category].push(item)
-			return acc
-		}, {})
-		res.json({
-			code: 200,
-			trip: {
-				id: resp._id,
-				name: resp.name,
-				users: resp.users,
-				owner: resp.owner,
-				duration: resp.duration,
-				equipList: filteredByCategory
-			}
-		})
-	}
+	const equipList = await EquipsListModel.findById({ _id: resp.equipList })
 	res.json({
 		code: 200,
-		trip: resp
+		trip: {
+			id: resp._id,
+			name: resp.name,
+			users: resp.users,
+			owner: resp.owner,
+			duration: resp.duration,
+			equipList: equipList.list
+		}
 	})
 }
 
