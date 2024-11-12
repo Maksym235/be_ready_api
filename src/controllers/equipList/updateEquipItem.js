d43428fd358dc35984b4611a8b3adc84f9d706a6
@m.andriichuk <andriichukwork235@gmail.com>
2024-11-12 17:57:52 +0200

    adds func upd count

--- src/controllers/equipList/updateEquipItem.js
+++ src/controllers/equipList/updateEquipItem.js
@@ -3,8 +3,9 @@ const { HttpError } = require('../../helpers')
 
 const updateEquipItem = async (req, res) => {
 	const { _id: userId } = req.user
-	const { equipId, category } = req.body
+	const { equipId, category, count: newCount } = req.body
 	const { id: listId } = req.params
+	const { updCount } = req.query
 	const list = await EquipsListModel.findById({ _id: listId })
 	if (!list) {
 		throw new HttpError(404, 'list not found')
@@ -16,22 +17,11 @@ const updateEquipItem = async (req, res) => {
 		(item) => String(item._id) === String(equipId)
 	)
 
-	if (listItem.persons.find((el) => String(el._id) === String(userId))) {
-		const updatedPersons = listItem.persons.filter(
-			(item) => String(item._id) !== String(userId)
-		)
-		// console.log(updatedPersons)
-		const newItem = {
-			_id: listItem._id,
-			name: listItem.name,
-			description: listItem.description,
-			persons: updatedPersons
-		}
-
-		newCategory.splice(index, 1, newItem)
-
-		newList.set(category, newCategory)
-
+	if (!listItem.persons.find((el) => String(el._id) === String(userId))) {
+		listItem.persons.push({
+			_id: userId,
+			count: 1
+		})
 		await EquipsListModel.findByIdAndUpdate(
 			{ _id: listId },
 			{
@@ -46,10 +36,50 @@ const updateEquipItem = async (req, res) => {
 			message: 'Success updated'
 		})
 	}
-	listItem.persons.push({
-		_id: userId,
-		count: 1
-	})
+	console.log(updCount)
+	console.log(typeof JSON.parse(updCount))
+	if (JSON.parse(updCount)) {
+		const person = listItem.persons.find(
+			(el) => String(el._id) === String(userId)
+		)
+		const personIndex = listItem.persons.findIndex(
+			(el) => String(el._id) === String(userId)
+		)
+		const newPerson = {
+			_id: person._id,
+			count: newCount
+		}
+		listItem.persons.splice(personIndex, 1, newPerson)
+
+		await EquipsListModel.findByIdAndUpdate(
+			{ _id: listId },
+			{
+				list: newList
+			},
+			{
+				new: true
+			}
+		)
+		res.json({
+			code: 200,
+			message: 'Success count updated'
+		})
+	}
+	const updatedPersons = listItem.persons.filter(
+		(item) => String(item._id) !== String(userId)
+	)
+	// console.log(updatedPersons)
+	const newItem = {
+		_id: listItem._id,
+		name: listItem.name,
+		description: listItem.description,
+		persons: updatedPersons
+	}
+
+	newCategory.splice(index, 1, newItem)
+
+	newList.set(category, newCategory)
+
 	await EquipsListModel.findByIdAndUpdate(
 		{ _id: listId },
 		{
@@ -63,6 +93,7 @@ const updateEquipItem = async (req, res) => {
 		code: 200,
 		message: 'Success updated'
 	})
+
 	// const equipItem = list.list.find((item) => String(item._id) === equipId)
 	// const isIncludeUser = equipItem.persons.find(
 	// 	(el) => String(el._id) === userId
